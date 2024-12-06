b8190b6968124ea86650c0e70fd63a2983351190
Nick Reck <nicholasreck@outlook.com>
2024-12-05 20:10:55 -0500

    test comparator

--- day5/part2.go
+++ day5/part2.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"slices"
@@ -10,88 +9,27 @@ import (
 )
 
 func part2() {
-	file, _ := os.Open("input.txt")
-	r := bufio.NewReader(file)
+  input, _ := os.ReadFile("input.txt")
+  split := strings.Split(strings.TrimSpace(string(input)), "\n\n")
 
-	rules := make(map[int][]int)
-	for {
-		orderingRulesRaw, err := r.ReadString('\n')
-		orderingRulesRaw = strings.TrimSuffix(orderingRulesRaw, "\n")
-		if err != nil || orderingRulesRaw == "" {
-			break
-		}
+  comparator := func(x, y string) int {
+    for _, v := range strings.Split(split[0], "\n") {
+      rule := strings.Split(v, "|")
+      if rule[0] == x && rule[1] == y {
+        return -1
+      }
+    }
+    return 0
+  }
 
-		sli := strings.Split(orderingRulesRaw, "|")
-		before, _ := strconv.Atoi(sli[0])
-		after, _ := strconv.Atoi(sli[1])
-		beforeList := rules[before]
-		if beforeList == nil {
-			rules[before] = []int{after}
-		} else {
-			rules[before] = append(beforeList, after)
-		}
-	}
-
-	sum := 0
-	for {
-		listForValidation, err := r.ReadString('\n')
-		listForValidation = strings.TrimSuffix(listForValidation, "\n")
-		if err != nil || listForValidation == "" {
-			break
-		}
-
-		sli := strings.Split(listForValidation, ",")
-		valid := true
-		for i := len(sli) - 1; i > 0 && valid; i-- {
-			val, _ := strconv.Atoi(sli[i])
-			constraints := rules[val]
-			for j := i - 1; j >= 0 && valid; j-- {
-				check, _ := strconv.Atoi(sli[j])
-				if slices.Index(constraints, check) != -1 {
-					valid = false
-				}
-			}
-		}
-
-		if valid {
-			continue
-		}
-
-		ordered := fixOrdering(sli, &rules)
-
-		index := len(ordered) / 2
-		sum += ordered[index]
-	}
-
-	fmt.Println(sum)
-}
-
-func fixOrdering(sli []string, m *map[int][]int) []int {
-	ordered := make([]int, 0)
-	for i := 0; i < len(sli); i++ {
-		min := len(ordered)
-		val, _ := strconv.Atoi(sli[i])
-		constraints := (*m)[val]
-		for _, c := range constraints {
-			index := slices.Index(ordered, c)
-			if index != -1 && min > index {
-				min = index
-			}
-		}
-		if min == len(ordered) {
-			ordered = append(ordered, val)
-		} else {
-			ordered = slices.Insert(ordered, min, val)
-		}
-	}
-	return ordered
-}
-
-func tryAndPlace(int *[]int, val, index int) {
-	if (*int)[index] == 0 {
-		(*int)[index] = val
-		return
-	}
-	index--
-	tryAndPlace(int, val, index)
+  count := 0
+  for _, v := range strings.Split(split[1], "\n") {
+    sli := strings.Split(v, ",")
+    if !slices.IsSortedFunc(sli, comparator) {
+      slices.SortFunc(sli, comparator)
+      add, _ := strconv.Atoi(sli[len(sli)/2])
+      count += add
+    }
+  }
+  fmt.Println(count)
 }
